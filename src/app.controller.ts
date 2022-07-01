4ab20ce75073f6ed12361efb2526e3e3321ab718
AlisonPLC <alisonpietro@gmail.com>
2022-07-01 16:53:31 -0300

    feat(endpoints): Criado endpoints necessarios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Patch } from '@nestjs/common';
 import { AppService } from './app.service';
-
+//endpoints
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,68 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  getTest() {
+    return this.appService.getTest();
+  }
+
+  //Lista de Controlers da Categoria
+  @Get('/categorias')
+  getCategorias() {
+    return this.appService.getCategorias();
+  }
+  @Get('/categorias/:id')
+  getCategoriasId() {
+    return this.appService.getCategoriasId();
+  }
+  @Post('/categorias/:id')
+  postCategorias() {
+    return this.appService.postCategorias();
+  }
+  @Patch('/categorias/:id')
+  patchCategorias() {
+    return this.appService.patchCategorias();
+  }
+  @Delete('/categorias/:id')
+  deleteCategories() {
+    return this.appService.deleteCategorias();
+  }
+
+  //Lista de Controlers da Produtos
+  @Get('/produtos')
+  getProdutos() {
+    return this.appService.getProdutos();
+  }
+  @Get('/produtos/:id')
+  getProdutosID() {
+    return this.appService.getProdutosId();
+  }
+  @Post('/produtos/:id')
+  postProdutos() {
+    return this.appService.postProdutos();
+  }
+  @Patch('/produtos/:id')
+  patchProdutos() {
+    return this.appService.patchProdutos();
+  }
+  @Delete('/produtos/:id')
+  deleteProdutos() {
+    return this.appService.deleteProdutos();
+  }
+
+  //lista de Itens a serem implementadas na tabela ESTOQUE
+  @Get('/produtos/:id/estoque')
+  getProdutosIDEstoque() {
+    return this.appService.getProdutosIdEsqotue();
+  }
+  @Patch('/produtos/:id/estoque')
+  patchProdutosEstoque() {
+    return this.appService.patchProdutosIdEstoque();
+  }
+  @Delete('/produtos/:id/estoque')
+  deleteProdutosEstoque() {
+    return this.appService.deleteProdutosIdEstoque();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello From 3005!';
+    return 'Hello World!';
   }
+  getTest(): string {
+    return `teste de categorias`;
+  }
+
+  //lista de Itens a serem implementadas na tabela CATEGORIA
+
+  getCategorias() {
+    return '/categoria';
+  } // Lista Todas As Categorias
+  getCategoriasId() {
+    return '/categoria/:ID';
+  } //Busca uma Categoria por ID
+  postCategorias() {
+    return '/categoria/:ID';
+  } // Cria Uma Categoria
+  patchCategorias() {
+    return '/categoria/:ID';
+  } //Edita Uma Categoria
+  deleteCategorias() {
+    return '/categoria/:ID';
+  } //Deleta uma Categoria (deve atualizar o produto setando idCategoria como NULL para produtos que utilizam essa categoria)
+
+  //lista de Itens a serem implementadas na tabela Produtos
+
+  getProdutos() {
+    return '/produtos';
+  } // Lista Todos os Produtos
+  getProdutosId() {
+    return '/produtos/:ID';
+  } //Busca uma Produtos por ID
+  postProdutos() {
+    return '/produtos';
+  } // Cria Um Produtos
+  patchProdutos() {
+    return '/produtos';
+  } //Edita Uma Produtos
+  deleteProdutos() {
+    return '/produtos';
+  } //Deleta uma Produtos (deve atualizar o produto setando idProdutos como NULL para produtos que utilizam essa Produtos)
+
+  //lista de Itens a serem implementadas na tabela ESTOQUE
+
+  //* Quando um produto é criado, deve ser criado um estoque com quantidade 0 para o Produto
+  //* Só pode haver 1 estoque para um mesmo Produto
+
+  getProdutosIdEsqotue() {
+    return '/produtos/:id/estoque';
+  } //Lista o estoque para o Produto pelo id
+  patchProdutosIdEstoque() {
+    return '/produtos/:id/estoque';
+  } //Edita o Estoque para o Produto pelo id
+  deleteProdutosIdEstoque() {
+    return HttpStatus.NOT_IMPLEMENTED;
+  } // Deve retornar o status [501] - Not Implemented. (não se pode deletar um estoque)
 }
